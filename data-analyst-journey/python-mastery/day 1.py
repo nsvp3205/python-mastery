8e6beeb95569423847b6189a0d0f65373f52a17b
nagasaivaraprasad <nsvp3205@gmail.com>
2025-12-23 23:18:33 +0530

    dec 23: python day-1

---
+++ data-analyst-journey/python-mastery/day 1.py
@@ -0,0 +1,19 @@
+# day 1 variables and data types practice
+
+
+# 1. string (text)
+name = "Naga sai vara prasad"
+goal_role = "data_analyst"
+
+# 2. integer (whole number)
+months_to_graduate = 6
+daily_coding_hours = 3
+
+# 3. float (decimal number)
+current_cgpa = 7.5
+
+# 4. boolean (True or False)
+is_graduated = False
+has_pushed_code_to_github_today = True
+is_enrolled_in_nxtwave = True
+
